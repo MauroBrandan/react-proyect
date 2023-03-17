06d0464620940a20d0ce9884f7375e3df0e48dec
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-03-16 21:01:21 -0300

    Mockup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Grid, Header } from 'semantic-ui-react'
 
 function App() {
   return (
-    <div className="App">
-      ToDo App
-    </div>
+  <>
+    <Header as='h1' color='teal'>
+      My to do list
+    </Header>
+    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 550 }}>
+      {/* Progress Bar */}        
+      {/* Search */}        
+      {/* To do List */}      
+      </Grid.Column>
+    </Grid>
+    {/* Add To do */} 
+  </>
   );
 }
 
