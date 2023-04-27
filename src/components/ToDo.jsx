9779091e81b4f3e03898c9aeffd8a7415b065b6a
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-04-26 21:52:05 -0300

    Add delete button to ToDo component

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import { List } from 'semantic-ui-react'
 
-export const ToDo = ({todoTitle, todoStatus}) => {
-  return (
-    <List.Item>
-        {todoStatus
-          ? <List.Icon name='check square' color='green' size='large'/>
-          : <List.Icon name='check square outline' size='large'/>}
-        <List.Content>
-            {todoTitle}
-        </List.Content>
-    </List.Item>
-  )
+export const ToDo = ({ todoTitle, todoStatus }) => {
+	return (
+		<List.Item>
+			{todoStatus ? (
+				<List.Icon
+					name='check square'
+					color='green'
+					size='large'
+					style={{ cursor: 'pointer' }}
+				/>
+			) : (
+				<List.Icon
+					name='check square outline'
+					size='large'
+					style={{ cursor: 'pointer' }}
+				/>
+			)}
+			<List.Content>{todoTitle}</List.Content>
+			<List.Icon
+				name='delete'
+				color='red'
+				size='small'
+				style={{ cursor: 'pointer' }}
+			/>
+		</List.Item>
+	)
 }
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,15 +4,15 @@ import { List } from 'semantic-ui-react'
 import { ToDo } from './ToDo'
 
 const toDos = [
-    {title: 'Wake Up', status: true},
-    {title: 'Read', status: false},
-    {title: 'Coffee', status: true}
+	{ title: 'Wake Up', status: true },
+	{ title: 'Read', status: false },
+	{ title: 'Coffee', status: true },
 ]
 
 export const ToDoList = () => (
-    <List animated verticalAlign='middle' size='huge' divided relaxed>
-        {toDos.map(({title, status}) => (
-            <ToDo key={title} todoTitle={title} todoStatus={status} />
-        ))}
-    </List>
+	<List verticalAlign='middle' size='huge' divided relaxed>
+		{toDos.map(({ title, status }) => (
+			<ToDo key={title} todoTitle={title} todoStatus={status} />
+		))}
+	</List>
 )
